93e59cb254f17f4373cc3be0efb6734a79d9a740
wjbuena <wilsonjeffrey.buena@synpulse.com>
2022-07-18 13:29:43 +0800

    Added object types to schema

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP());
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
 app.listen(4000, () => {
     console.log("Now ready to listen to requests on port 4000");
