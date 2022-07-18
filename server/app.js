b3dd52e10d7aa7516f93fe57f2c5095634de68d8
wjbuena <wilsonjeffrey.buena@synpulse.com>
2022-07-18 10:15:50 +0800

    test

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', graphqlHTTP());
+
 app.listen(4000, () => {
     console.log("Now ready to listen to requests on port 4000");
 });
